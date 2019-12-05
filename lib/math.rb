e1ee7dcc9d21b4f42b78d78a7177e2dd2c315a09
Michael Diaz <mike.diaz006@gmail.com>
2019-12-05 23:29:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,24 +7,29 @@ end
 def subtraction(num1, num2)
 subtraction = num1 - num2
 print subtraction
+return subtraction
 end
 
 def division(num1, num2)
 division = num1 / num2
 print division
+return division
 end
 
 def multiplication(num1, num2)
 multiplication = num1 * num2
 print multiplication
+return multiplication
 end
 
 def modulo(num1, num2)
 mod = num1 % num2
 print mod
+return mod
 end
 
 def square_root(num)
 square_root = Math.sqrt(num)
 print square_root
+return mod
 end
